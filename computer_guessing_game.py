f04eec04850c8a10b04fb63ff0aab3101e4ef383
LechX <lech.kaiel@gmail.com>
2016-11-30 21:34:08 -0800

    halfway there, still having global variable issues

--- computer_guessing_game.py
+++ computer_guessing_game.py
@@ -12,6 +12,7 @@ guess_count = 0
 upper_bound = 100
 lower_bound = 1
 comp_number = 0
+user_number = 0
 
 def generate_user_number(upper_bound, lower_bound):
     """
@@ -21,25 +22,18 @@ def generate_user_number(upper_bound, lower_bound):
     """
     user_number = int(input("Hi! Choose a number between 1 and 100! >> "))
     return user_number
-
     if user_number < lower_bound or user_number > upper_bound:
         user_number = int(input("Your number is outside the parameters. Please choose a different number. >> "))
 
-
-
-
-
-def computer_guess(upper_bound, lower_bound):
+def computer_guess(comp_number, guess_count):
     """
     input: upper and lower bounds as integers
     usage: generate computer guess, starting at 1 and incrementing by 1
     output: computer's guess as integer, increments guess_count by 1
     """
-
-    comp_number += 1
+    comp_number = guess_count + 1
     return comp_number
     guess_count += 1
-
 
 def check_number(user_number, comp_number, upper_bound, lower_bound):
     """
@@ -56,16 +50,16 @@ def check_number(user_number, comp_number, upper_bound, lower_bound):
             lower_bound = comp_number + 1
         return True
 
-def main(upper_bound, lower_bound):
+def main(upper_bound, lower_bound, user_number, comp_number, guess_count):
     """
     input: upper and lower bounds
     usage: calls all functions necessary to execute guessing game
     output: print statement describing how many guesses the comp needed
     """
     generate_user_number(upper_bound, lower_bound)
-    computer_guess(upper_bound, lower_bound)
+    computer_guess(comp_number, guess_count)
     while check_number(user_number, comp_number, upper_bound, lower_bound):
         computer_guess(upper_bound, lower_bound)
     print("It took the computer {} guesses to find {}.".format(guess_count, user_number))
 
-main(upper_bound, lower_bound)
+main(upper_bound, lower_bound, user_number, comp_number, guess_count)
